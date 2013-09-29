280f58a8fc0d63b7b28157f7cf892c4680bc3297
Israel Calvete <icalvete@gmail.com>
2013-09-29 11:49:15 +0200

    Support for CentOS|RedHat

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,10 +1,5 @@
 class rabbitmq::install {
 
-  exec {'install_erlangs':
-    command => '/usr/bin/apt-get -q -y -o DPkg::Options::=--force-confold install erlang-nox',
-    unless  => '/usr/bin/dpkg -l erlang-nox | grep ii'
-  }
-
   common::down_resource {'rabbitmq_get_package':
     scheme   => $rabbitmq::repo_scheme,
     domain   => $rabbitmq::repo_domain,
@@ -13,20 +8,42 @@ class rabbitmq::install {
     pass     => $rabbitmq::repo_pass,
     path     => $rabbitmq::repo_path,
     resource => $rabbitmq::repo_resource,
-    require  => Exec['install_erlangs'],
   }
 
-  exec {'rabbitmq_install_package':
-    cwd     => '/tmp/',
-    command => "/usr/bin/dpkg -i ${rabbitmq::repo_resource}",
-    require => Common::Down_resource['rabbitmq_get_package'],
-    unless  => '/usr/bin/dpkg -l rabbitmq-server | grep ii'
+  case $::operatingsystem {
+    /^(Debian|Ubuntu)$/: {
+
+      exec {'install_erlangs':
+        command => '/usr/bin/apt-get -q -y -o DPkg::Options::=--force-confold install erlang-nox',
+        unless  => '/usr/bin/dpkg -l erlang-nox | grep ii'
+      }
+
+      exec {'rabbitmq_install_package':
+        cwd     => '/tmp/',
+        command => "/usr/bin/dpkg -i ${rabbitmq::repo_resource}",
+        require => Common::Down_resource['rabbitmq_get_package'],
+        unless  => '/usr/bin/dpkg -l rabbitmq-server | grep ii'
+      }
+
+      exec {'rabbitmq_install':
+        cwd     => '/tmp/',
+        command => '/usr/bin/apt-get -f install',
+        require => Exec['rabbitmq_install_package'],
+        unless  => '/usr/bin/dpkg -l rabbitmq-server | grep ii'
+      }
+
+    }
+    /^(CentOS|RedHat)$/: {
+
+      realize Package['erlang']
+
+      exec {'rabbitmq_install_package':
+        cwd     => '/tmp/',
+        command => "/bin/rpm -i ${rabbitmq::repo_resource}",
+        require => Common::Down_resource['rabbitmq_get_package'],
+      }
+
+    }
   }
 
-  exec {'rabbitmq_install':
-    cwd     => '/tmp/',
-    command => '/usr/bin/apt-get -f install',
-    require => Exec['rabbitmq_install_package'],
-    unless  => '/usr/bin/dpkg -l rabbitmq-server | grep ii'
-  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,16 +1,16 @@
 class rabbitmq::params {
 
-  case $::operatingsystem {
-    /^(Debian|Ubuntu)$/: {
+  $repo_scheme = hiera('sp_repo_scheme')
+  $repo_domain = hiera('sp_repo_domain')
+  $repo_port   = hiera('sp_repo_port')
+  $repo_user   = hiera('sp_repo_user')
+  $repo_pass   = hiera('sp_repo_pass')
+  $repo_path   = hiera('sp_repo_path')
+  $package     = hiera('rabbitmq_package')
 
-      $repo_scheme = hiera('sp_repo_scheme')
-      $repo_domain = hiera('sp_repo_domain')
-      $repo_port   = hiera('sp_repo_port')
-      $repo_user   = hiera('sp_repo_user')
-      $repo_pass   = hiera('sp_repo_pass')
-      $repo_path   = hiera('sp_repo_path')
-      $package     = hiera('rabbitmq_package')
-    }
+  case $::operatingsystem {
+    /^(Debian|Ubuntu)$/: {}
+    /^(CentOS|RedHat)$/: {}
     default: {
       fail ("${::operatingsystem} not supported.")
     }
